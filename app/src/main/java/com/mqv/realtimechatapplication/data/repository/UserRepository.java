a9edbab190eebc51a75c522dfab6ccb8d5788b85
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-08 22:35:54 +0700

    add fetch remote user with callback

--- app/src/main/java/com/mqv/realtimechatapplication/data/repository/UserRepository.java
+++ app/src/main/java/com/mqv/realtimechatapplication/data/repository/UserRepository.java
@@ -1,8 +1,21 @@
 package com.mqv.realtimechatapplication.data.repository;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.mqv.realtimechatapplication.network.ApiResponse;
+import com.mqv.realtimechatapplication.network.model.User;
+
+import java.util.function.Consumer;
+
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.Response;
 
 public interface UserRepository {
     Observable<Response<String>> addUser(String uid);
+
+    /*
+     * Using this method will help us easy to use from another ViewModel
+     * */
+    void fetchUserFromRemote(User remoteUser,
+                             FirebaseUser user,
+                             Consumer<Observable<ApiResponse<User>>> callback);
 }
--- app/src/main/java/com/mqv/realtimechatapplication/data/repository/UserRepositoryImpl.java
+++ app/src/main/java/com/mqv/realtimechatapplication/data/repository/UserRepositoryImpl.java
@@ -1,13 +1,22 @@
 package com.mqv.realtimechatapplication.data.repository;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.mqv.realtimechatapplication.network.ApiResponse;
+import com.mqv.realtimechatapplication.network.model.User;
 import com.mqv.realtimechatapplication.network.service.UserService;
+import com.mqv.realtimechatapplication.util.Const;
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.Response;
 
-public class UserRepositoryImpl implements UserRepository{
+public class UserRepositoryImpl implements UserRepository {
     private final UserService userService;
 
     @Inject
@@ -18,5 +27,32 @@ public class UserRepositoryImpl implements UserRepository{
     @Override
     public Observable<Response<String>> addUser(String uid) {
         return userService.addUser(uid);
+    }
+
+    @Override
+    public void fetchUserFromRemote(User remoteUser,
+                                    @NonNull FirebaseUser user,
+                                    @NonNull Consumer<Observable<ApiResponse<User>>> callback) {
+        /*
+         * Target remote user uid. If null it's mean clients want to load themself account information
+         * */
+        var uid = remoteUser != null ? remoteUser.getUid() : user.getUid();
+
+        // Required Firebase User token to make sure clients are using the app to make a call
+        user.getIdToken(true)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        var token = Objects.requireNonNull(task.getResult()).getToken();
+
+                        callback.accept(userService.fetchUserFromRemote(
+                                Const.PREFIX_TOKEN + token,
+                                Const.DEFAULT_AUTHORIZER,
+                                uid));
+                    } else {
+                        callback.accept(null);
+                        var e = task.getException();
+                        Objects.requireNonNull(e).printStackTrace();
+                    }
+                });
     }
 }
