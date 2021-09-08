e6533efca23317a994b4167c0f8d87562f89a0cb
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-08 22:34:13 +0700

    add composite disposable

--- app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/FirebaseUserViewModel.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/FirebaseUserViewModel.java
@@ -7,8 +7,11 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+
 public abstract class FirebaseUserViewModel extends ViewModel {
     protected final MutableLiveData<FirebaseUser> firebaseUser = new MutableLiveData<>();
+    protected final CompositeDisposable cd = new CompositeDisposable();
 
     protected void loadFirebaseUser(){
         var user = FirebaseAuth.getInstance().getCurrentUser();
@@ -17,5 +20,11 @@ public abstract class FirebaseUserViewModel extends ViewModel {
 
     public LiveData<FirebaseUser> getFirebaseUser() {
         return firebaseUser;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        cd.dispose();
     }
 }
