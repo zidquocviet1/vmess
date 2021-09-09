52c88be169b1e8e7b5a6882d4c30556cd6c2e07a
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-09 23:29:23 +0700

    add key to using sparse array

--- app/src/main/java/com/mqv/realtimechatapplication/network/model/Gender.java
+++ app/src/main/java/com/mqv/realtimechatapplication/network/model/Gender.java
@@ -4,25 +4,31 @@ import com.google.gson.annotations.SerializedName;
 
 public enum Gender {
     @SerializedName("male")
-    MALE("Male"),
+    MALE("Male", 1),
     @SerializedName("female")
-    FEMALE("Female"),
+    FEMALE("Female", 2),
     @SerializedName("nonBinary")
-    NON_BINARY("Non-binary"),
+    NON_BINARY("Non-binary", 3),
     @SerializedName("transgender")
-    TRANSGENDER("Transgender"),
+    TRANSGENDER("Transgender", 4),
     @SerializedName("intersex")
-    INTERSEX("Intersex"),
+    INTERSEX("Intersex", 5),
     @SerializedName("preferNotToSay")
-    PREFER_NOT_TO_SAY("Prefer not to say");
+    PREFER_NOT_TO_SAY("Prefer not to say", 6);
 
     private final String value;
+    private final int key;
 
-    Gender(String value) {
+    Gender(String value, int key) {
         this.value = value;
+        this.key = key;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public int getKey(){
+        return key;
     }
 }
