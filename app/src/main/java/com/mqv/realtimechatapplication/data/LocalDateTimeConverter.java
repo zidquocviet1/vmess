a80a5a9e9207efdf74a82b2908c4038c88674653
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-09 22:59:57 +0700

    add room type converter for local date time type

---
+++ app/src/main/java/com/mqv/realtimechatapplication/data/LocalDateTimeConverter.java
@@ -0,0 +1,21 @@
+package com.mqv.realtimechatapplication.data;
+
+import androidx.room.TypeConverter;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeConverter {
+    @TypeConverter
+    public LocalDateTime fromTimestamp(String value) {
+        return value == null ? null : LocalDateTime.parse(value);
+    }
+
+    @TypeConverter
+    public String dateToTimestamp(LocalDateTime date) {
+        if (date == null) {
+            return null;
+        } else {
+            return date.toString();
+        }
+    }
+}
