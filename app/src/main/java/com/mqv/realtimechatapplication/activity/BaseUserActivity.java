3677415cf36eefc7b3be18e1eba3858241a14d57
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-10 23:49:32 +0700

    add the BaseUser class that is used for observe User changed

---
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/BaseUserActivity.java
@@ -0,0 +1,29 @@
+package com.mqv.realtimechatapplication.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
+import androidx.lifecycle.ViewModel;
+import androidx.viewbinding.ViewBinding;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.mqv.realtimechatapplication.network.model.User;
+
+public abstract class BaseUserActivity<V extends ViewModel, B extends ViewBinding> extends ToolbarActivity<V, B>{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        registerFirebaseUserChange(this::refreshFirebaseUserUi);
+
+        registerLoggedInUserChanged(this::refreshLoggedInUserUi);
+    }
+
+    @UiThread
+    public abstract void refreshFirebaseUserUi(FirebaseUser user);
+
+    @UiThread
+    public abstract void refreshLoggedInUserUi(User user);
+}
