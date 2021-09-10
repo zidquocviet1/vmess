0f5f9d7b7a7e4e93191328d44c582b601864c4da
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-10 16:03:30 +0700

    make it become entity in room

--- app/src/main/java/com/mqv/realtimechatapplication/network/model/User.java
+++ app/src/main/java/com/mqv/realtimechatapplication/network/model/User.java
@@ -1,25 +1,33 @@
 package com.mqv.realtimechatapplication.network.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.time.LocalDateTime;
 
+@Entity(tableName = "user")
 public class User {
+    @PrimaryKey
+    @NonNull
     private String uid;
     private String biographic;
     private Gender gender;
     private LocalDateTime birthday;
 
-    public User(String uid, String biographic, Gender gender, LocalDateTime birthday) {
+    public User(@NonNull String uid, String biographic, Gender gender, LocalDateTime birthday) {
         this.uid = uid;
         this.biographic = biographic;
         this.gender = gender;
         this.birthday = birthday;
     }
 
+    @NonNull
     public String getUid() {
         return uid;
     }
 
-    public void setUid(String uid) {
+    public void setUid(@NonNull String uid) {
         this.uid = uid;
     }
 
