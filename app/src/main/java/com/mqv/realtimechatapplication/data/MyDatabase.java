b01fcfc6a1825c25e217bead0be85543e342f572
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-10 16:01:37 +0700

    change entity class and add type converters

--- app/src/main/java/com/mqv/realtimechatapplication/data/MyDatabase.java
+++ app/src/main/java/com/mqv/realtimechatapplication/data/MyDatabase.java
@@ -2,11 +2,15 @@ package com.mqv.realtimechatapplication.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import com.mqv.realtimechatapplication.data.converter.GenderConverter;
+import com.mqv.realtimechatapplication.data.converter.LocalDateTimeConverter;
 import com.mqv.realtimechatapplication.data.dao.UserDao;
-import com.mqv.realtimechatapplication.data.model.LoggedInUser;
+import com.mqv.realtimechatapplication.network.model.User;
 
-@Database(entities = {LoggedInUser.class}, version = 1)
+@Database(entities = {User.class}, version = 1)
+@TypeConverters({LocalDateTimeConverter.class, GenderConverter.class})
 public abstract class MyDatabase extends RoomDatabase {
     public abstract UserDao getUserDao();
 }
