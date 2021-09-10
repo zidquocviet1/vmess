f1da3a4e1a5b0e8f103602cc6ca7ee115eb2c1f9
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-10 16:01:10 +0700

    add some method to get and insert user

--- app/src/main/java/com/mqv/realtimechatapplication/data/dao/UserDao.java
+++ app/src/main/java/com/mqv/realtimechatapplication/data/dao/UserDao.java
@@ -1,7 +1,23 @@
 package com.mqv.realtimechatapplication.data.dao;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.mqv.realtimechatapplication.network.model.User;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Single;
 
 @Dao
 public interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable save(User user);
+
+    @Query("select * from user")
+    Flowable<List<User>> findByUid();
 }
