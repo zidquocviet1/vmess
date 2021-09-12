d6e06eb0a5bd6a6dbd9c72f6fccd3b084f34e1e5
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-12 22:57:49 +0700

    remove unused code

--- app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/EditProfileViewModel.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/EditProfileViewModel.java
@@ -1,57 +1,20 @@
 package com.mqv.realtimechatapplication.activity.viewmodel;
 
-import androidx.annotation.Nullable;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
-import com.mqv.realtimechatapplication.R;
 import com.mqv.realtimechatapplication.data.repository.UserRepository;
-import com.mqv.realtimechatapplication.data.result.Result;
-import com.mqv.realtimechatapplication.network.model.User;
-
-import java.net.HttpURLConnection;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @HiltViewModel
 public class EditProfileViewModel extends CurrentUserViewModel {
     private final UserRepository userRepository;
-    private final MutableLiveData<Result<User>> remoteUser = new MutableLiveData<>();
 
     @Inject
     public EditProfileViewModel(UserRepository userRepository) {
         this.userRepository = userRepository;
 
         loadFirebaseUser();
-    }
-
-    public LiveData<Result<User>> getRemoteUser() {
-        return remoteUser;
-    }
-
-    public void fetchUserFromRemote(@Nullable User remoteUser) {
-        var user = firebaseUser.getValue();
-
-        if (user != null) {
-            userRepository.fetchUserFromRemote(remoteUser, user, observable -> {
-                if (observable != null) {
-                    cd.add(observable.subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(response -> {
-                                if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
-                                    this.remoteUser.setValue(Result.Success(response.getSuccess()));
-                                } else if (response.getStatusCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                                    this.remoteUser.setValue(Result.Fail(R.string.error_authentication_fail));
-                                }
-                            }, t -> this.remoteUser.setValue(Result.Fail(R.string.error_connect_server_fail))));
-                } else {
-                    this.remoteUser.setValue(Result.Fail(R.string.error_authentication_fail));
-                }
-            });
-        }
+        loadLoggedInUser();
     }
 }
