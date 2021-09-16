6ad66a0bbb047e1abdec7bd075d5c9927292169d
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-16 23:46:17 +0700

    insert package manager and url to open profile page from other app

--- app/src/main/java/com/mqv/realtimechatapplication/network/model/SocialType.java
+++ app/src/main/java/com/mqv/realtimechatapplication/network/model/SocialType.java
@@ -4,33 +4,38 @@ import android.util.SparseArray;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+
 public enum SocialType {
     @SerializedName("instagram")
-    INSTAGRAM("instagram", 1),
+    INSTAGRAM("instagram", 1, "com.instagram.android", "https://instagram.com/_u/"),
     @SerializedName("facebook")
-    FACEBOOK("facebook", 2),
+    FACEBOOK("facebook", 2, "com.facebook.katana", "https://www.facebook.com/"),
     @SerializedName("github")
-    GITHUB("github", 3),
+    GITHUB("github", 3, "", "https://github.com/"),
     @SerializedName("linkedin")
-    LINKEDIN("linkedin", 4),
+    LINKEDIN("linkedin", 4, "com.linkedin.android", "https://www.linkedin.com/in/"),
     @SerializedName("pinterest")
-    PINTEREST("pinterest", 5),
+    PINTEREST("pinterest", 5, "", "https://www.pinterest.com/"),
     @SerializedName("soundcloud")
-    SOUNDCLOUD("soundcloud", 6),
+    SOUNDCLOUD("soundcloud", 6, "", "https://soundcloud.com/"),
     @SerializedName("tiktok")
-    TIKTOK("tiktok", 7),
+    TIKTOK("tiktok", 7, "com.zhiliaoapp.musically", "https://www.tiktok.com/"),
     @SerializedName("tumblr")
-    TUMBLR("tumblr", 8),
+    TUMBLR("tumblr", 8, "", "https://www.tumblr.com/"),
     @SerializedName("twitter")
-    TWITTER("twitter", 9),
+    TWITTER("twitter", 9, "com.twitter.android", "https://twitter.com/"),
     @SerializedName("vine")
-    VINE("vine", 10),
+    VINE("vine", 10, "", "https://vine.co/"),
     @SerializedName("vk")
-    VK("vk", 11);
+    VK("vk", 11, "", "https://vk.com/");
 
     private final String value;
+    private final String packageName;
+    private final String url;
     private final int key;
     private static final SparseArray<SocialType> array = new SparseArray<>();
+
     static {
         array.put(1, INSTAGRAM);
         array.put(2, FACEBOOK);
@@ -45,20 +50,40 @@ public enum SocialType {
         array.put(11, VK);
     }
 
-    SocialType(String value, int key) {
+    SocialType(String value, int key, String packageName, String url) {
         this.value = value;
         this.key = key;
+        this.packageName = packageName;
+        this.url = url;
     }
 
     public String getValue() {
         return value;
     }
 
-    public int getKey(){
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getKey() {
         return key;
     }
 
-    public static SocialType getGenderByKey(int key){
+    public static SocialType getSocialTypeByKey(int key) {
         return array.get(key);
+    }
+
+    public static ArrayList<SocialType> getSocialTypeAsArray() {
+        var result = new ArrayList<SocialType>();
+
+        for (int i = 1; i <= array.size(); i++) {
+            result.add(array.get(i));
+        }
+
+        return result;
     }
 }
