ee39255297af255fae0dfb1bc34fea1013e5645f
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-16 14:20:05 +0700

    add util loading dialog

---
+++ app/src/main/java/com/mqv/realtimechatapplication/util/LoadingDialog.java
@@ -0,0 +1,36 @@
+package com.mqv.realtimechatapplication.util;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.mqv.realtimechatapplication.R;
+
+public class LoadingDialog {
+    private static AlertDialog loadingDialog;
+
+    public static void startLoadingDialog(Context context, LayoutInflater inflater, int content) {
+        var builder = new MaterialAlertDialogBuilder(context);
+        var view = inflater.inflate(R.layout.dialog_loading_with_text, null, false);
+        var textUploading = (TextView) view.findViewById(R.id.text_uploading);
+        var animBlink = AnimationUtils.loadAnimation(context, R.anim.blink);
+
+        textUploading.setText(content);
+        textUploading.startAnimation(animBlink);
+        builder.setView(view);
+
+        loadingDialog = builder.create();
+        loadingDialog.setCancelable(false);
+        loadingDialog.setCanceledOnTouchOutside(false);
+        loadingDialog.show();
+    }
+
+    public static void finishLoadingDialog() {
+        if (loadingDialog != null && loadingDialog.isShowing())
+            loadingDialog.dismiss();
+    }
+}
