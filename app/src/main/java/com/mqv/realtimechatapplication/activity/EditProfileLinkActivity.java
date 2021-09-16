cd2f62b8657e5338c308a2ddf89a6a8c1ef25bdc
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-16 23:03:00 +0700

    update profile link

--- app/src/main/java/com/mqv/realtimechatapplication/activity/EditProfileLinkActivity.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/EditProfileLinkActivity.java
@@ -5,29 +5,54 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.mqv.realtimechatapplication.R;
 import com.mqv.realtimechatapplication.activity.viewmodel.EditProfileLinkViewModel;
 import com.mqv.realtimechatapplication.databinding.ActivityEditProfileLinkBinding;
+import com.mqv.realtimechatapplication.network.model.SocialType;
 import com.mqv.realtimechatapplication.network.model.UserSocialLink;
 import com.mqv.realtimechatapplication.ui.adapter.UserLinkAdapter;
+import com.mqv.realtimechatapplication.ui.fragment.SocialLinkListDialogFragment;
+import com.mqv.realtimechatapplication.util.LoadingDialog;
 import com.mqv.realtimechatapplication.util.Logging;
+import com.mqv.realtimechatapplication.util.NetworkStatus;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkViewModel, ActivityEditProfileLinkBinding> implements View.OnClickListener {
+public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkViewModel, ActivityEditProfileLinkBinding>
+        implements View.OnClickListener, SocialLinkListDialogFragment.OnSocialPlatformSelected,
+        UserLinkAdapter.OnEditSocialLinkItemListener {
     private UserLinkAdapter mAdapter;
-    private EditText editAccountName;
+    private SocialType mSelectedSocialType;
+    private Button mSelectedButton;
     private List<UserSocialLink> mOldLinkList;
+    private final List<UserSocialLink> mNewLinkList = new ArrayList<>();
+    private ArrayList<SocialType> socialTypeArrayList;
+
+    public static final String ARG_ACTIVITY_CALLER = "activity_caller";
+    public static final String ARG_ADAPTER_CALLER = "adapter_caller";
+
+    private OnSocialSelectedFromAdapterListener callback;
+
+    public interface OnSocialSelectedFromAdapterListener {
+        void onSocialSelected(Button button, SocialType type);
+    }
+
+    public void setOnSocialSelectedFromAdapterListener(OnSocialSelectedFromAdapterListener callback) {
+        this.callback = callback;
+    }
 
     @Override
     public void binding() {
@@ -49,7 +74,9 @@ public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkView
 
         setupRecyclerView();
 
-        editAccountName = Objects.requireNonNull(mBinding.textLayoutAccountName.getEditText());
+        socialTypeArrayList = SocialType.getSocialTypeAsArray();
+
+        var editAccountName = Objects.requireNonNull(mBinding.textLayoutAccountName.getEditText());
         editAccountName.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 var imm = getSystemService(InputMethodManager.class);
@@ -60,31 +87,58 @@ public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkView
                 var accountName = v.getText().toString().trim();
 
                 if (!TextUtils.isEmpty(accountName)) {
-                    Logging.show(accountName);
+                    var newUserSocialLink = new UserSocialLink(System.currentTimeMillis(), mSelectedSocialType, accountName);
+
+                    mAdapter.addItem(newUserSocialLink);
                 }
+
+                v.setText("");
+                mSelectedSocialType = null;
+                mBinding.buttonSelectService.setText(R.string.action_select_platform);
+                mBinding.textLayoutAccountName.setEnabled(false);
                 return true;
             }
             return false;
         });
         mBinding.buttonSave.setOnClickListener(this);
         mBinding.buttonAddSocialLink.setOnClickListener(this);
+        mBinding.buttonSelectService.setOnClickListener(this);
     }
 
     @Override
     public void setupObserver() {
         mViewModel.getUserSocialLinkList().observe(this, links -> {
-            mOldLinkList = links;
-            mAdapter.submitList(links);
+            mOldLinkList = links == null ? new ArrayList<>() : links.stream()
+                    .map(u -> new UserSocialLink(u.getId(), u.getType(), u.getAccountName()))
+                    .collect(Collectors.toList());
+
+            mNewLinkList.addAll(links == null ? new ArrayList<>() : links);
+            mAdapter.submitList(mNewLinkList);
         });
 
         mViewModel.getUpdateResult().observe(this, result -> {
+            if (result == null) return;
 
+            if (result.getStatus() == NetworkStatus.LOADING) {
+                LoadingDialog.startLoadingDialog(this, getLayoutInflater(), R.string.action_loading);
+            } else if (result.getStatus() == NetworkStatus.SUCCESS) {
+                LoadingDialog.finishLoadingDialog();
+
+                updateLoggedInUser(result.getSuccess());
+
+                Toast.makeText(this, R.string.msg_update_user_info_successfully, Toast.LENGTH_SHORT).show();
+
+                finish();
+            } else {
+                LoadingDialog.finishLoadingDialog();
+
+                Toast.makeText(this, result.getError(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
     private void setupRecyclerView() {
-        mAdapter = new UserLinkAdapter(this, R.layout.item_individual_social_link, UserLinkAdapter.ACTION.EDIT);
-
+        mAdapter = new UserLinkAdapter(this, mNewLinkList, R.layout.item_individual_social_link, UserLinkAdapter.ACTION.EDIT);
         mBinding.recyclerEditLink.setAdapter(mAdapter);
         mBinding.recyclerEditLink.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
@@ -113,7 +167,7 @@ public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkView
                         .collect(Collectors.toList());
 
                 var keyNewList = newLinkList.stream()
-                        .map(link -> link.getId() == null ? -1 : link.getId())
+                        .map(UserSocialLink::getId)
                         .collect(Collectors.toList());
 
                 // Compare to list {https://stackoverflow.com/questions/2762093/java-compare-two-lists}
@@ -133,6 +187,8 @@ public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkView
             }
         } else if (id == mBinding.buttonAddSocialLink.getId()) {
             changeUiState(true);
+        } else if (id == mBinding.buttonSelectService.getId()) {
+            showSocialLinkFragmentDialog(ARG_ACTIVITY_CALLER);
         }
     }
 
@@ -140,5 +196,39 @@ public class EditProfileLinkActivity extends ToolbarActivity<EditProfileLinkView
         mBinding.buttonAddSocialLink.setVisibility(isSelectService ? View.GONE : View.VISIBLE);
         mBinding.buttonSelectService.setVisibility(isSelectService ? View.VISIBLE : View.GONE);
         mBinding.textLayoutAccountName.setVisibility(isSelectService ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onPlatformSelected(int position) {
+        mSelectedSocialType = (position < 0 || position >= socialTypeArrayList.size()) ? null : socialTypeArrayList.get(position);
+        var socialName = mSelectedSocialType != null ? mSelectedSocialType.getValue() : getString(R.string.action_select_platform);
+        var capitalizeName = socialName.substring(0, 1).toUpperCase(Locale.ROOT) + socialName.substring(1);
+        mBinding.buttonSelectService.setText(capitalizeName);
+        mBinding.textLayoutAccountName.setEnabled(mSelectedSocialType != null);
+    }
+
+    @Override
+    public void onPlatformSelectedFromAdapter(int position) {
+        mSelectedSocialType = (position < 0 || position >= socialTypeArrayList.size())
+                ? null : socialTypeArrayList.get(position);
+        if (callback != null) {
+            callback.onSocialSelected(mSelectedButton, mSelectedSocialType);
+        }
+    }
+
+    @Override
+    public void onOpenPlatformSelection(Button button, UserSocialLink item) {
+        showSocialLinkFragmentDialog(ARG_ADAPTER_CALLER);
+        mSelectedButton = button;
+    }
+
+    private void showSocialLinkFragmentDialog(String caller) {
+        var socialTypeNameList = socialTypeArrayList.stream()
+                .map(SocialType::getValue)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        var socialLinkListBottomSheetFragment =
+                SocialLinkListDialogFragment.newInstance(socialTypeNameList, caller);
+        socialLinkListBottomSheetFragment.show(getSupportFragmentManager(), "Social List");
     }
 }
