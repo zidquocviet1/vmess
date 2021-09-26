ff02f52165ac5d4665d103f221ec611f86dcf5bd
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-26 21:25:42 +0700

    provide friend request repository

--- app/src/main/java/com/mqv/realtimechatapplication/di/RepositoryModule.java
+++ app/src/main/java/com/mqv/realtimechatapplication/di/RepositoryModule.java
@@ -4,12 +4,15 @@ import com.mqv.realtimechatapplication.data.dao.HistoryLoggedInUserDao;
 import com.mqv.realtimechatapplication.data.dao.UserDao;
 import com.mqv.realtimechatapplication.data.repository.EditUserPhotoRepository;
 import com.mqv.realtimechatapplication.data.repository.EditUserPhotoRepositoryImpl;
+import com.mqv.realtimechatapplication.data.repository.FriendRequestRepository;
+import com.mqv.realtimechatapplication.data.repository.FriendRequestRepositoryImpl;
 import com.mqv.realtimechatapplication.data.repository.HistoryLoggedInUserRepository;
 import com.mqv.realtimechatapplication.data.repository.HistoryLoggedInUserRepositoryImpl;
 import com.mqv.realtimechatapplication.data.repository.LoginRepository;
 import com.mqv.realtimechatapplication.data.repository.LoginRepositoryImpl;
 import com.mqv.realtimechatapplication.data.repository.UserRepository;
 import com.mqv.realtimechatapplication.data.repository.UserRepositoryImpl;
+import com.mqv.realtimechatapplication.network.service.FriendRequestService;
 import com.mqv.realtimechatapplication.network.service.UserService;
 
 import javax.inject.Singleton;
@@ -24,7 +27,7 @@ import dagger.hilt.components.SingletonComponent;
 public class RepositoryModule {
     @Provides
     @Singleton
-    public LoginRepository provideLoginRes(UserService service, UserDao userDao, HistoryLoggedInUserDao historyLoggedInUserDao){
+    public LoginRepository provideLoginRes(UserService service, UserDao userDao, HistoryLoggedInUserDao historyLoggedInUserDao) {
         return new LoginRepositoryImpl(service, userDao, historyLoggedInUserDao);
     }
 
@@ -32,19 +35,25 @@ public class RepositoryModule {
     @Singleton
     public EditUserPhotoRepository provideEditUserPhotoRes(UserService service,
                                                            HistoryLoggedInUserDao historyUserDao,
-                                                           UserDao userDao){
+                                                           UserDao userDao) {
         return new EditUserPhotoRepositoryImpl(service, historyUserDao, userDao);
     }
 
     @Provides
     @Singleton
-    public UserRepository provideUserRepository(UserService service, UserDao userDao){
+    public UserRepository provideUserRepository(UserService service, UserDao userDao) {
         return new UserRepositoryImpl(service, userDao);
     }
 
     @Provides
     @Singleton
-    public HistoryLoggedInUserRepository provideHistoryUserRepo(HistoryLoggedInUserDao historyLoggedInUserDao){
+    public HistoryLoggedInUserRepository provideHistoryUserRepo(HistoryLoggedInUserDao historyLoggedInUserDao) {
         return new HistoryLoggedInUserRepositoryImpl(historyLoggedInUserDao);
+    }
+
+    @Singleton
+    @Provides
+    public FriendRequestRepository provideFriendRequestRepo(FriendRequestService service) {
+        return new FriendRequestRepositoryImpl(service);
     }
 }
