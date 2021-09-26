17cb3691f5180aedeb9b1b2d3516d3045de73fc3
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-26 21:22:42 +0700

    refresh firebase every time app is opened

--- app/src/main/java/com/mqv/realtimechatapplication/activity/SplashScreenActivity.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/SplashScreenActivity.java
@@ -1,22 +1,39 @@
 package com.mqv.realtimechatapplication.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.lifecycle.AndroidViewModel;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.mqv.realtimechatapplication.R;
+import com.mqv.realtimechatapplication.databinding.ActivitySplashScreenBinding;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SplashScreenActivity extends BaseActivity<AndroidViewModel, ActivitySplashScreenBinding> {
+
+    @Override
+    public void binding() {
+        mBinding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public Class<AndroidViewModel> getViewModelClass() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
         var user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null){
+            // TODO: check user login session
+
+            reloadFirebaseUser();
+
             var intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         }else {
@@ -24,5 +41,10 @@ public class SplashScreenActivity extends AppCompatActivity {
         }
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         finishAfterTransition();
+    }
+
+    @Override
+    public void setupObserver() {
+
     }
 }
