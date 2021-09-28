bff7316a440fa13419ebcf091361a6c6faaa867f
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-28 22:08:54 +0700

    final get connect people by username

--- app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/UsernameConnectionFragment.java
+++ app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/UsernameConnectionFragment.java
@@ -1,66 +1,127 @@
 package com.mqv.realtimechatapplication.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.mqv.realtimechatapplication.R;
+import com.mqv.realtimechatapplication.activity.RequestPeopleActivity;
+import com.mqv.realtimechatapplication.activity.viewmodel.ConnectPeopleViewModel;
+import com.mqv.realtimechatapplication.databinding.FragmentUsernameConnectionBinding;
+import com.mqv.realtimechatapplication.util.NetworkStatus;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsernameConnectionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class UsernameConnectionFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class UsernameConnectionFragment extends BaseFragment<ConnectPeopleViewModel, FragmentUsernameConnectionBinding>
+        implements TextWatcher, View.OnClickListener {
+    private static final int MAX_USER_NAME_LENGTH = 20;
 
     public UsernameConnectionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsernameConnectionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UsernameConnectionFragment newInstance(String param1, String param2) {
-        UsernameConnectionFragment fragment = new UsernameConnectionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void binding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        mBinding = FragmentUsernameConnectionBinding.inflate(inflater, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public Class<ConnectPeopleViewModel> getViewModelClass() {
+        return ConnectPeopleViewModel.class;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_username_connection, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBinding.textPromptLength.setText(getString(R.string.prompt_bio_length, 0, MAX_USER_NAME_LENGTH));
+        mBinding.editUserName.addTextChangedListener(this);
+        mBinding.buttonBack.setOnClickListener(this);
+        mBinding.buttonFind.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mViewModel.removeUsernameDisposable();
+    }
+
+    @Override
+    public void setupObserver() {
+        mViewModel.getConnectUserResult().observe(getViewLifecycleOwner(), result -> {
+            if (result == null)
+                return;
+
+            var status = result.getStatus();
+            var isLoading = status == NetworkStatus.LOADING;
+
+            mBinding.progressBarLoading.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            mBinding.buttonFind.setEnabled(!isLoading);
+            mBinding.editUserName.setEnabled(!isLoading);
+
+            switch (status) {
+                case SUCCESS:
+                    var user = result.getSuccess();
+                    var firebaseUser = mViewModel.getFirebaseUser().getValue();
+
+                    if (firebaseUser != null) {
+                        if (user.getUid().equals(firebaseUser.getUid())) {
+                            Toast.makeText(requireContext(), R.string.msg_request_yourself, Toast.LENGTH_SHORT).show();
+                        } else {
+                            var intent = new Intent(requireActivity(), RequestPeopleActivity.class);
+                            intent.putExtra("user", user);
+                            startActivity(intent);
+                            requireActivity().finish();
+                        }
+                    }
+                    break;
+                case ERROR:
+                    Toast.makeText(requireContext(), result.getError(), Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+            mViewModel.resetConnectUserResult();
+        });
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        mBinding.buttonFind.setEnabled(s.length() != 0);
+        mBinding.textPromptLength.setText(getString(R.string.prompt_bio_length, s.length(), MAX_USER_NAME_LENGTH));
+    }
+
+    @Override
+    public void onClick(View v) {
+        var id = v.getId();
+
+        if (id == mBinding.buttonBack.getId()) {
+            requireActivity().onBackPressed();
+        } else if (id == mBinding.buttonFind.getId()) {
+            var username = mBinding.editUserName.getText().toString().trim();
+
+            mViewModel.getConnectUserByUsername(username);
+        }
     }
 }
