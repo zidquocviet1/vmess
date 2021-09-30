02fc388c464ae87970879258916365363c8d88a1
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-09-30 22:43:11 +0700

    response pending requested status from PreferenceFriendRequestActivity

--- app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/FriendRequestViewModel.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/viewmodel/FriendRequestViewModel.java
@@ -20,12 +20,12 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 @HiltViewModel
 public class FriendRequestViewModel extends CurrentUserViewModel {
     private final MutableLiveData<Result<List<FriendRequest>>> friendRequestList = new MutableLiveData<>();
+    private final MutableLiveData<Result<Boolean>> responseRequestResult = new MutableLiveData<>();
     private final FriendRequestRepository repository;
 
     @Inject
     public FriendRequestViewModel(FriendRequestRepository repository) {
         this.repository = repository;
-        loadFirebaseUser();
 
         loadPendingFriendRequest();
     }
@@ -34,26 +34,51 @@ public class FriendRequestViewModel extends CurrentUserViewModel {
         return friendRequestList;
     }
 
+    public LiveData<Result<Boolean>> getResponseRequestResult() {
+        return responseRequestResult;
+    }
+
     private void loadPendingFriendRequest() {
-        var firebaseUser = getFirebaseUser().getValue();
+        friendRequestList.setValue(Result.Loading());
 
-        if (firebaseUser != null) {
-            friendRequestList.setValue(Result.Loading());
+        repository.getAllPendingRequest(observable -> cd.add(observable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(response -> {
+                            if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
+                                friendRequestList.setValue(Result.Success(response.getSuccess()));
+                            } else if (response.getStatusCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                                friendRequestList.setValue(Result.Fail(R.string.error_authentication_fail));
+                            }
+                        }, t -> friendRequestList.setValue(Result.Fail(R.string.error_connect_server_fail)))),
+                e -> friendRequestList.setValue(Result.Fail(R.string.error_authentication_fail)));
+    }
 
-            repository.getAllPendingRequest(firebaseUser,
-                    observable -> cd.add(observable
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(response -> {
-                                if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
-                                    friendRequestList.setValue(Result.Success(response.getSuccess()));
-                                } else if (response.getStatusCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                                    friendRequestList.setValue(Result.Fail(R.string.error_authentication_fail));
-                                }
-                            }, t -> friendRequestList.setValue(Result.Fail(R.string.error_connect_server_fail)))),
-                    e -> friendRequestList.setValue(Result.Fail(R.string.error_authentication_fail)));
-        } else {
-            friendRequestList.setValue(Result.Fail(R.string.error_unknown));
-        }
+    public void responseFriendRequest(FriendRequest request) {
+        responseRequestResult.setValue(Result.Loading());
+
+        repository.responseFriendRequest(request,
+                observable -> cd.add(observable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(response -> {
+                            var code = response.getStatusCode();
+
+                            if (code == HttpURLConnection.HTTP_OK) {
+                                responseRequestResult.setValue(Result.Success(response.getSuccess()));
+                            } else if (code == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                                responseRequestResult.setValue(Result.Fail(R.string.error_authentication_fail));
+                            } else if (code == HttpURLConnection.HTTP_CONFLICT) {
+                                responseRequestResult.setValue(Result.Fail(R.string.error_response_request_conflict));
+                            }
+                        }, t -> {
+                            var message = t.getMessage();
+
+                            if (message != null && message.equals("HTTP 404 ")) {
+                                responseRequestResult.setValue(Result.Fail(R.string.error_user_id_not_found));
+                            } else
+                                responseRequestResult.setValue(Result.Fail(R.string.error_connect_server_fail));
+                        })),
+                e -> responseRequestResult.setValue(Result.Fail(R.string.error_authentication_fail)));
     }
 }
