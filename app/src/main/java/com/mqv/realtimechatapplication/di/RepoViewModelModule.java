56fca475ef682331fc5bb77209b953a2872a1b7c
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-02 21:59:28 +0700

    inject friend request service

--- app/src/main/java/com/mqv/realtimechatapplication/di/RepoViewModelModule.java
+++ app/src/main/java/com/mqv/realtimechatapplication/di/RepoViewModelModule.java
@@ -3,6 +3,7 @@ package com.mqv.realtimechatapplication.di;
 import com.mqv.realtimechatapplication.data.dao.PeopleDao;
 import com.mqv.realtimechatapplication.data.repository.PeopleRepository;
 import com.mqv.realtimechatapplication.data.repository.impl.PeopleRepositoryImpl;
+import com.mqv.realtimechatapplication.network.service.FriendRequestService;
 import com.mqv.realtimechatapplication.network.service.UserService;
 
 import dagger.Module;
@@ -16,7 +17,8 @@ import dagger.hilt.android.scopes.ViewModelScoped;
 public class RepoViewModelModule {
     @ViewModelScoped
     @Provides
-    public PeopleRepository providePeopleRepository(PeopleDao peopleDao, UserService userService){
-        return new PeopleRepositoryImpl(peopleDao, userService);
+    public PeopleRepository providePeopleRepository(PeopleDao peopleDao, UserService userService,
+                                                    FriendRequestService friendRequestService){
+        return new PeopleRepositoryImpl(peopleDao, userService, friendRequestService);
     }
 }
