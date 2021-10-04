3a1ccba756a0040b83863b6259d4322890c13b04
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-04 23:45:52 +0700

    add util class to get await firebase user token

---
+++ app/src/main/java/com/mqv/realtimechatapplication/util/UserTokenUtil.java
@@ -0,0 +1,26 @@
+package com.mqv.realtimechatapplication.util;
+
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+public final class UserTokenUtil {
+    public static Optional<String> getToken(@NonNull FirebaseUser user) {
+        String token = null;
+        try {
+            var tokenResult = Tasks.await(user.getIdToken(true));
+            token = tokenResult.getToken();
+        } catch (ExecutionException e) {
+            Logging.show("Was interrupted while waiting for the token.");
+        } catch (InterruptedException e) {
+            Logging.show("Failed to get the token." + e.getCause());
+        }
+        return Optional.ofNullable(TextUtils.isEmpty(token) ? null : token);
+    }
+}
