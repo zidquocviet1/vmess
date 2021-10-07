84ed7b5c5b286a6d647e28e812a81fe53b2d48e4
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-07 14:44:24 +0700

    update conversation fragment

--- app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/ConversationFragment.java
+++ app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/ConversationFragment.java
@@ -1,8 +1,6 @@
 package com.mqv.realtimechatapplication.ui.fragment;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,12 +18,8 @@ import com.mqv.realtimechatapplication.network.model.Conversation;
 import com.mqv.realtimechatapplication.ui.adapter.ConversationAdapter;
 import com.mqv.realtimechatapplication.ui.adapter.RankUserConversationAdapter;
 import com.mqv.realtimechatapplication.ui.fragment.viewmodel.ConversationFragmentViewModel;
-import com.mqv.realtimechatapplication.util.Logging;
-import com.mqv.realtimechatapplication.util.MessageStatus;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ConversationFragment extends BaseSwipeFragment<ConversationFragmentViewModel, FragmentConversationBinding>{
     private ConversationAdapter adapter;
@@ -51,15 +45,19 @@ public class ConversationFragment extends BaseSwipeFragment<ConversationFragment
     @Override
     public void setupObserver() {
         if (mViewModel != null){
-            mViewModel.getConversationList().observe(this, conversations -> {
+            mViewModel.getConversationListSafe().observe(this, conversations -> {
                 if (conversations != null && !conversations.isEmpty()){
                     adapter.submitList(conversations);
+
+                    stopRefresh();
                 }
             });
 
-            mViewModel.getRemoteUserList().observe(this, remoteUsers -> {
+            mViewModel.getRemoteUserListSafe().observe(this, remoteUsers -> {
                 if (remoteUsers != null && !remoteUsers.isEmpty()){
                     rankUserConversationAdapter.submitList(remoteUsers);
+
+                    stopRefresh();
                 }
             });
         }
@@ -73,18 +71,7 @@ public class ConversationFragment extends BaseSwipeFragment<ConversationFragment
 
     @Override
     public void onRefresh() {
-        Logging.show("onReload is called");
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            var listConversation = Arrays.asList(
-                    new Conversation(1L, "Phạm Thảo Duyên", "You: ok", LocalDateTime.now(), MessageStatus.RECEIVED),
-                    new Conversation(2L, "Phạm Băng Băng", "You: haha", LocalDateTime.now(), MessageStatus.SEEN),
-                    new Conversation(3L, "Triệu Lệ Dĩnh", "You: wo ai ni", LocalDateTime.now(), MessageStatus.SEEN),
-                    new Conversation(4L, "Ngô Diệc Phàm", "You: 2 phut hon", LocalDateTime.now(), MessageStatus.NOT_RECEIVED),
-                    new Conversation(5L, "Lưu Đức Hoa", "You: hao le", LocalDateTime.now(), MessageStatus.RECEIVED)
-            );
-            adapter.submitList(listConversation);
-            stopRefresh();
-        }, 2000);
+        mViewModel.onRefresh();
     }
 
     private void setupRecyclerView(){
--- app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/viewmodel/ConversationFragmentViewModel.java
+++ app/src/main/java/com/mqv/realtimechatapplication/ui/fragment/viewmodel/ConversationFragmentViewModel.java
@@ -1,70 +1,56 @@
 package com.mqv.realtimechatapplication.ui.fragment.viewmodel;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+import android.os.Handler;
+import android.os.Looper;
 
+import androidx.lifecycle.LiveData;
+
+import com.mqv.realtimechatapplication.activity.viewmodel.AbstractMainViewModel;
+import com.mqv.realtimechatapplication.data.repository.FriendRequestRepository;
+import com.mqv.realtimechatapplication.data.repository.PeopleRepository;
+import com.mqv.realtimechatapplication.data.repository.UserRepository;
 import com.mqv.realtimechatapplication.network.model.Conversation;
 import com.mqv.realtimechatapplication.network.model.RemoteUser;
-import com.mqv.realtimechatapplication.util.Logging;
 import com.mqv.realtimechatapplication.util.MessageStatus;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-public class ConversationFragmentViewModel extends ViewModel {
-    private final MutableLiveData<List<Conversation>> conversationList;
-    private final MutableLiveData<List<RemoteUser>> remoteUserList;
+import javax.inject.Inject;
 
-    public ConversationFragmentViewModel() {
-        conversationList = new MutableLiveData<>();
-        remoteUserList = new MutableLiveData<>();
+import dagger.hilt.android.lifecycle.HiltViewModel;
 
-        newData();
+@HiltViewModel
+public class ConversationFragmentViewModel extends AbstractMainViewModel {
+
+    @Inject
+    public ConversationFragmentViewModel(UserRepository userRepository,
+                                         FriendRequestRepository friendRequestRepository,
+                                         PeopleRepository peopleRepository) {
+        super(userRepository, friendRequestRepository, peopleRepository);
     }
 
-    public LiveData<List<Conversation>> getConversationList() {
-        return conversationList;
+    @Override
+    public void onRefresh() {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            var listConversation = Arrays.asList(
+                    new Conversation(1L, "Phạm Thảo Duyên", "You: ok", LocalDateTime.now(), MessageStatus.RECEIVED),
+                    new Conversation(2L, "Phạm Băng Băng", "You: haha", LocalDateTime.now(), MessageStatus.SEEN),
+                    new Conversation(3L, "Triệu Lệ Dĩnh", "You: wo ai ni", LocalDateTime.now(), MessageStatus.SEEN),
+                    new Conversation(4L, "Ngô Diệc Phàm", "You: 2 phut hon", LocalDateTime.now(), MessageStatus.NOT_RECEIVED),
+                    new Conversation(5L, "Lưu Đức Hoa", "You: hao le", LocalDateTime.now(), MessageStatus.RECEIVED)
+            );
+
+            getConversationList().postValue(listConversation);
+        }, 2000);
     }
 
-    public LiveData<List<RemoteUser>> getRemoteUserList() {
-        return remoteUserList;
+    public LiveData<List<Conversation>> getConversationListSafe(){
+        return getConversationList();
     }
 
-    /// test
-    public void newData() {
-        var listConversation = Arrays.asList(
-                new Conversation(1L, "Phạm Thảo Duyên", "You: ok", LocalDateTime.now(), MessageStatus.RECEIVED),
-                new Conversation(2L, "Phạm Băng Băng", "You: haha", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(3L, "Triệu Lệ Dĩnh", "You: wo ai ni", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(4L, "Ngô Diệc Phàm", "You: 2 phut hon", LocalDateTime.now(), MessageStatus.NOT_RECEIVED),
-                new Conversation(5L, "Lưu Đức Hoa", "You: hao le", LocalDateTime.now(), MessageStatus.RECEIVED),
-                new Conversation(6L, "Pham thao duyen 1", "You: ok", LocalDateTime.now(), MessageStatus.RECEIVED),
-                new Conversation(7L, "Pham Bang Bang 1", "You: haha", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(8L, "Trieu Le Dinh 1", "You: wo ai ni", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(9L, "Ngo Diec Pham 1", "You: 2 phut hon", LocalDateTime.now(), MessageStatus.NOT_RECEIVED),
-                new Conversation(10L, "Luu Duc Hoa 1", "You: hao le", LocalDateTime.now(), MessageStatus.RECEIVED),
-                new Conversation(11L, "Pham thao duyen 2", "You: ok", LocalDateTime.now(), MessageStatus.RECEIVED),
-                new Conversation(12L, "Pham Bang Bang 2", "You: haha", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(13L, "Trieu Le Dinh 2", "You: wo ai ni", LocalDateTime.now(), MessageStatus.SEEN),
-                new Conversation(14L, "Ngo Diec Pham 2", "You: 2 phut hon", LocalDateTime.now(), MessageStatus.NOT_RECEIVED),
-                new Conversation(15L, "Luu Duc Hoa 2", "You: hao le", LocalDateTime.now(), MessageStatus.RECEIVED)
-        );
-
-        var listRemoteUser = Arrays.asList(
-                new RemoteUser("1", "Thảo Duyên", "Phạm"),
-                new RemoteUser("2", "Băng Băng", "Phạm"),
-                new RemoteUser("3", "Lệ Dĩnh", "Triệu"),
-                new RemoteUser("4", "Diệc Phàm", "Ngô"),
-                new RemoteUser("5", "Đức Hoa", "Lưu"),
-                new RemoteUser("6", "Viet", "Mai"),
-                new RemoteUser("7", "Han", "Ngoc"),
-                new RemoteUser("8", "Nhu", "Tran"),
-                new RemoteUser("9", "Mai", "Phuong"),
-                new RemoteUser("10", "Tuyen", "Huyen")
-        );
-        conversationList.setValue(listConversation);
-        remoteUserList.setValue(listRemoteUser);
+    public LiveData<List<RemoteUser>> getRemoteUserListSafe(){
+        return getRemoteUserList();
     }
 }
