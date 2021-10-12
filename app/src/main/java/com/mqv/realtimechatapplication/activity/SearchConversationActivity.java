dce9248f54cfe147b9e54e24bd3f86afe16e7216
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-12 23:00:40 +0700

    add new activities

---
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/SearchConversationActivity.java
@@ -0,0 +1,16 @@
+package com.mqv.realtimechatapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mqv.realtimechatapplication.R;
+
+public class SearchConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_conversation);
+    }
+}
