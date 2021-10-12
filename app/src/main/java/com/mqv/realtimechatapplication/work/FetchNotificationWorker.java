d2c0ea56ae36f0cec6aa9bc03349f346af8dbfb7
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-12 15:26:22 +0700

    if recent login not run FetchNotificationWorker

--- app/src/main/java/com/mqv/realtimechatapplication/work/FetchNotificationWorker.java
+++ app/src/main/java/com/mqv/realtimechatapplication/work/FetchNotificationWorker.java
@@ -9,16 +9,21 @@ import androidx.work.WorkerParameters;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.mqv.realtimechatapplication.R;
+import com.mqv.realtimechatapplication.data.dao.HistoryLoggedInUserDao;
 import com.mqv.realtimechatapplication.data.dao.NotificationDao;
+import com.mqv.realtimechatapplication.data.dao.UserDao;
+import com.mqv.realtimechatapplication.data.model.HistoryLoggedInUser;
 import com.mqv.realtimechatapplication.network.ApiResponse;
 import com.mqv.realtimechatapplication.network.exception.FirebaseUnauthorizedException;
 import com.mqv.realtimechatapplication.network.model.Notification;
+import com.mqv.realtimechatapplication.network.model.User;
 import com.mqv.realtimechatapplication.network.service.NotificationService;
 import com.mqv.realtimechatapplication.util.Const;
 import com.mqv.realtimechatapplication.util.Logging;
 import com.mqv.realtimechatapplication.util.UserTokenUtil;
 
 import java.net.HttpURLConnection;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +32,9 @@ import dagger.assisted.AssistedInject;
 import io.reactivex.rxjava3.core.CompletableObserver;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.observers.DisposableSingleObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @HiltWorker
@@ -36,47 +43,30 @@ public class FetchNotificationWorker extends Worker {
 
     private final NotificationService service;
     private final NotificationDao dao;
+    private final UserDao userDao;
+    private final HistoryLoggedInUserDao historyLoggedInUserDao;
 
     @AssistedInject
     public FetchNotificationWorker(@Assisted @NonNull Context context,
                                    @Assisted @NonNull WorkerParameters workerParams,
                                    NotificationService service,
-                                   NotificationDao dao) {
+                                   NotificationDao dao,
+                                   UserDao userDao,
+                                   HistoryLoggedInUserDao historyLoggedInUserDao) {
         super(context, workerParams);
         this.service = service;
         this.dao = dao;
+        this.userDao = userDao;
+        this.historyLoggedInUserDao = historyLoggedInUserDao;
     }
 
     @NonNull
     @Override
     public Result doWork() {
-        Logging.show("Start fetching new notification list");
+        Logging.show(TAG + ": Start fetching new notification list");
 
-        getNotificationObservable()
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+        shouldFetch();
 
-                    }
-
-                    @Override
-                    public void onNext(@NonNull ApiResponse<List<Notification>> response) {
-                        if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
-                            saveNotificationList(response.getSuccess());
-                        }
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Logging.show(TAG + ": Fetch notification list into from server error");
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Logging.show(TAG + ": Complete fetching notification list");
-                    }
-                });
         return Result.success();
     }
 
@@ -110,7 +100,7 @@ public class FetchNotificationWorker extends Worker {
                 .subscribeOn(Schedulers.io())
                 .subscribe(new CompletableObserver() {
                     @Override
-                    public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {
+                    public void onSubscribe(@NonNull Disposable d) {
 
                     }
 
@@ -120,8 +110,65 @@ public class FetchNotificationWorker extends Worker {
                     }
 
                     @Override
-                    public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+                    public void onError(@NonNull Throwable e) {
                         Logging.show(TAG + ": Save notification list into database error");
+                    }
+                });
+    }
+
+    private Single<HistoryLoggedInUser> getLoggedInUser() {
+        return historyLoggedInUserDao.getLoggedInUser()
+                .subscribeOn(Schedulers.io());
+    }
+
+    private void shouldFetch() {
+        getLoggedInUser()
+                .flatMap(user -> userDao.findByUid(user.getUid()))
+                .subscribe(new DisposableSingleObserver<>() {
+                    @Override
+                    public void onSuccess(@NonNull User user) {
+                        var fetchTimeOut = 30;
+                        var now = LocalDateTime.now();
+                        var shouldFetch = user.getAccessedDate().plusSeconds(fetchTimeOut).compareTo(now) <= 0;
+
+                        if (shouldFetch) {
+                            fetchNotification();
+                        }else{
+                            Logging.show(TAG + ": Recent login, not need to refresh new notification list");
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    private void fetchNotification() {
+        getNotificationObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ApiResponse<List<Notification>> response) {
+                        if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
+                            saveNotificationList(response.getSuccess());
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Logging.show(TAG + ": Fetch notification list into from server error");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Logging.show(TAG + ": Complete fetching notification list");
                     }
                 });
     }
