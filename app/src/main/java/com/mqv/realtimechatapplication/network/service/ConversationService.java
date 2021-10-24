87259b00f55956df14c2899d1e24bb407478499d
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-24 15:33:20 +0700

    add conversation service

---
+++ app/src/main/java/com/mqv/realtimechatapplication/network/service/ConversationService.java
@@ -0,0 +1,29 @@
+package com.mqv.realtimechatapplication.network.service;
+
+import static com.mqv.realtimechatapplication.util.Const.*;
+
+import com.mqv.realtimechatapplication.network.ApiResponse;
+import com.mqv.realtimechatapplication.network.model.Chat;
+import com.mqv.realtimechatapplication.network.model.Conversation;
+import com.mqv.realtimechatapplication.network.model.type.ConversationStatusType;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface ConversationService {
+    @GET("conversation")
+    Observable<ApiResponse<List<Conversation>>> fetchConversation(@Header(AUTHORIZATION) String token,
+                                                                  @Header(AUTHORIZER) String authorizer,
+                                                                  @Query("status") ConversationStatusType type);
+
+    @POST("conversation/add_chat")
+    Observable<ApiResponse<Chat>> sendMessage(@Header(AUTHORIZATION) String token,
+                                              @Header(AUTHORIZER) String authorizer,
+                                              @Body Chat chat);
+}
