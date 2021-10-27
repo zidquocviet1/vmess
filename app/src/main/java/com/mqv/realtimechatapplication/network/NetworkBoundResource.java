22eebf69ec9ade5fb7c46c2b36860ff11ccc3380
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-10-27 22:27:04 +0700

    update network bound resource

--- app/src/main/java/com/mqv/realtimechatapplication/network/NetworkBoundResource.java
+++ app/src/main/java/com/mqv/realtimechatapplication/network/NetworkBoundResource.java
@@ -31,8 +31,6 @@ public abstract class NetworkBoundResource<ResultType, RequestType> {
                             } else {
                                 makeCallFromRemote();
                             }
-                        } else {
-                            result = loadFromDb();
                         }
                     }
 
