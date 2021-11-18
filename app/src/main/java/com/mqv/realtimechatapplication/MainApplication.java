1a7318f6f5d9c02eb6ba27840a55e04a6241eee1
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-11-18 16:10:15 +0700

    register activity lifecycle callbacks to determine what exactly active activity

--- app/src/main/java/com/mqv/realtimechatapplication/MainApplication.java
+++ app/src/main/java/com/mqv/realtimechatapplication/MainApplication.java
@@ -1,14 +1,18 @@
 package com.mqv.realtimechatapplication;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.hilt.work.HiltWorkerFactory;
 import androidx.work.Configuration;
 
 import com.mqv.realtimechatapplication.activity.preferences.AppPreferences;
 import com.mqv.realtimechatapplication.activity.preferences.DarkMode;
+import com.mqv.realtimechatapplication.util.Logging;
 
 import javax.inject.Inject;
 
@@ -22,10 +26,13 @@ public class MainApplication extends Application implements Configuration.Provid
     @Inject
     HiltWorkerFactory workerFactory;
 
+    private Activity activeActivity;
+
     @Override
     public void onCreate() {
         super.onCreate();
         setAppTheme(mPreferences.getDarkModeTheme());
+        setupActivityListener();
     }
 
     private void setAppTheme(DarkMode mode){
@@ -48,5 +55,44 @@ public class MainApplication extends Application implements Configuration.Provid
         return new Configuration.Builder()
                 .setWorkerFactory(workerFactory)
                 .build();
+    }
+
+    private void setupActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                activeActivity = activity;
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+                activeActivity = null;
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+                Logging.show("onActivityDestroyed: " + activity.getLocalClassName());
+            }
+        });
+    }
+
+    public Activity getActiveActivity() {
+        return activeActivity;
     }
 }
