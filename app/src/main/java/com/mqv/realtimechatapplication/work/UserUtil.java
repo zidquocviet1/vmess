a2049cc4d5efb6f009070e02079b80b8c65080cc
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-12-29 22:50:23 +0700

    update user util class

--- app/src/main/java/com/mqv/realtimechatapplication/work/UserUtil.java
+++ app/src/main/java/com/mqv/realtimechatapplication/work/UserUtil.java
@@ -1,12 +1,22 @@
 package com.mqv.realtimechatapplication.work;
 
+import android.util.Pair;
+
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.FirebaseUser;
+import com.mqv.realtimechatapplication.R;
 import com.mqv.realtimechatapplication.data.dao.HistoryLoggedInUserDao;
 import com.mqv.realtimechatapplication.data.dao.UserDao;
+import com.mqv.realtimechatapplication.network.exception.FirebaseUnauthorizedException;
+import com.mqv.realtimechatapplication.util.Const;
+import com.mqv.realtimechatapplication.util.UserTokenUtil;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
 public final class UserUtil {
@@ -29,5 +39,36 @@ public final class UserUtil {
                                  return user.getAccessedDate().plusSeconds(fetchTimeOut).compareTo(now) <= 0;
                              })
                              .onErrorReturnItem(false);
+    }
+
+    public static Observable<String> getBearerTokenObservable(FirebaseUser user) {
+        return Observable.fromCallable(() -> {
+                    try {
+                        return new Pair<Optional<String>, Throwable>(UserTokenUtil.getToken(user), null);
+                    } catch (Throwable t) {
+                        return new Pair<>(Optional.<String>empty(), t);
+                    }
+                })
+                .flatMap(pair -> {
+                    Optional<String> tokenOptional = pair.first;
+                    Throwable throwable = pair.second;
+
+                    if (tokenOptional.isPresent()) {
+                        String token = tokenOptional.get();
+                        String bearerToken = Const.PREFIX_TOKEN + token;
+
+                        return Observable.just(bearerToken);
+                    } else {
+                        return Observable.create(emitter -> {
+                            if (!emitter.isDisposed()) {
+                                if (throwable instanceof FirebaseNetworkException) {
+                                    emitter.onError(new FirebaseNetworkException("Network error"));
+                                } else {
+                                    emitter.onError(new FirebaseUnauthorizedException(R.string.error_authentication_fail));
+                                }
+                            }
+                        });
+                    }
+                });
     }
 }
