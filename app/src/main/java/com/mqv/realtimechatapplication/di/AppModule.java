431b21ffe217f633b79c85f91d1f94e295618bbb
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-12-29 09:43:38 +0700

    inject websocket client

--- app/src/main/java/com/mqv/realtimechatapplication/di/AppModule.java
+++ app/src/main/java/com/mqv/realtimechatapplication/di/AppModule.java
@@ -1,7 +1,9 @@
 package com.mqv.realtimechatapplication.di;
 
+import com.google.gson.Gson;
 import com.mqv.realtimechatapplication.data.dao.HistoryLoggedInUserDao;
 import com.mqv.realtimechatapplication.data.dao.UserDao;
+import com.mqv.realtimechatapplication.network.websocket.WebSocketClient;
 import com.mqv.realtimechatapplication.work.UserUtil;
 
 import javax.inject.Singleton;
@@ -10,6 +12,7 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
 
 @Module
 @InstallIn(SingletonComponent.class)
@@ -18,5 +21,11 @@ public class AppModule {
     @Singleton
     public UserUtil provideUserUtil(UserDao userDao, HistoryLoggedInUserDao historyLoggedInUserDao) {
         return new UserUtil(userDao, historyLoggedInUserDao);
+    }
+
+    @Singleton
+    @Provides
+    public WebSocketClient provideWebSocketClient(OkHttpClient client, Gson gson) {
+        return new WebSocketClient(client, gson);
     }
 }
