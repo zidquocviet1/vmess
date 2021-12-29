533f567e3e06f1d8407db44336b09d132c012ece
Mai Quoc Viet <zid.quocviet200@gmail.com>
2021-12-29 22:59:55 +0700

    update BaseWorker

--- app/src/main/java/com/mqv/realtimechatapplication/work/BaseWorker.java
+++ app/src/main/java/com/mqv/realtimechatapplication/work/BaseWorker.java
@@ -3,14 +3,19 @@ package com.mqv.realtimechatapplication.work;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
+
+import java.util.UUID;
 
 public abstract class BaseWorker implements WorkerProperty {
     private final Context context;
     private final String workerName;
+    private WorkRequest request;
 
     private static final String DEFAULT_WORK_NAME = "BASE_WORK_" + System.currentTimeMillis();
 
@@ -27,7 +32,7 @@ public abstract class BaseWorker implements WorkerProperty {
     public abstract WorkRequest createRequest();
 
     public void run() {
-        WorkRequest request = createRequest();
+        request = createRequest();
         WorkManager manager = WorkManager.getInstance(context);
 
         if (isUniqueWork()) {
@@ -39,5 +44,13 @@ public abstract class BaseWorker implements WorkerProperty {
         } else {
             manager.enqueue(request);
         }
+    }
+
+    private UUID getWorkId() {
+        return request.getId();
+    }
+
+    public LiveData<WorkInfo> getWorkInfo() {
+        return WorkManager.getInstance(context).getWorkInfoByIdLiveData(getWorkId());
     }
 }
