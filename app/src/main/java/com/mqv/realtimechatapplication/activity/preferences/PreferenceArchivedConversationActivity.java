c12d16fd40ddbc263b6ae0d63c724229d37103fa
Mai Quoc Viet <zid.quocviet200@gmail.com>
2022-01-18 14:39:52 +0700

    separate the archived conversation activity to fragment

--- app/src/main/java/com/mqv/realtimechatapplication/activity/preferences/PreferenceArchivedConversationActivity.java
+++ app/src/main/java/com/mqv/realtimechatapplication/activity/preferences/PreferenceArchivedConversationActivity.java
@@ -1,34 +1,18 @@
 package com.mqv.realtimechatapplication.activity.preferences;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.mqv.realtimechatapplication.R;
-import com.mqv.realtimechatapplication.activity.ConversationActivity;
 import com.mqv.realtimechatapplication.activity.ToolbarActivity;
 import com.mqv.realtimechatapplication.activity.viewmodel.ConversationListArchivedViewModel;
 import com.mqv.realtimechatapplication.databinding.ActivityPreferenceArchivedChatBinding;
-import com.mqv.realtimechatapplication.network.model.Conversation;
-import com.mqv.realtimechatapplication.network.model.type.ConversationStatusType;
-import com.mqv.realtimechatapplication.ui.adapter.ConversationListAdapter;
-import com.mqv.realtimechatapplication.ui.fragment.ConversationDialogFragment;
-import com.mqv.realtimechatapplication.util.NetworkStatus;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
+import com.mqv.realtimechatapplication.ui.fragment.ConversationListArchivedFragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class PreferenceArchivedConversationActivity extends ToolbarActivity<ConversationListArchivedViewModel, ActivityPreferenceArchivedChatBinding>
-        implements ConversationDialogFragment.ConversationOptionListener {
-    private List<Conversation> mConversations = new ArrayList<>();
-    private ConversationListAdapter mAdapter;
+public class PreferenceArchivedConversationActivity extends ToolbarActivity<ConversationListArchivedViewModel,
+        ActivityPreferenceArchivedChatBinding> {
 
     @Override
     public void binding() {
@@ -48,133 +32,12 @@ public class PreferenceArchivedConversationActivity extends ToolbarActivity<Conv
 
         updateActionBarTitle(R.string.title_preference_item_archived_chats);
 
-        setupRecyclerView();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_conversation_list, ConversationListArchivedFragment.newInstance())
+                .commit();
     }
 
     @Override
     public void setupObserver() {
-        mViewModel.getArchivedChatResult().observe(this, result -> {
-            if (result == null) return;
-
-            boolean isLoading = result.getStatus() == NetworkStatus.LOADING;
-
-            mBinding.progressBarLoading.setVisibility(isLoading ? View.VISIBLE : View.GONE);
-            mBinding.recyclerViewArchivedChats.setVisibility(isLoading ? View.GONE : View.VISIBLE);
-
-            switch (result.getStatus()) {
-                case SUCCESS:
-                    mConversations = result.getSuccess() == null ? new ArrayList<>() : result.getSuccess();
-                    mAdapter.submitList(new ArrayList<>(mConversations));
-                    checkDataIsEmpty();
-                    break;
-                case ERROR:
-                    break;
-            }
-        });
-
-        mViewModel.getListObserver().observe(this, list -> {
-            if (list == null) return;
-
-            mConversations = list;
-            mAdapter.submitList(new ArrayList<>(mConversations));
-            checkDataIsEmpty();
-        });
-    }
-
-    private void setupRecyclerView() {
-        mAdapter = new ConversationListAdapter(mConversations, this);
-        mAdapter.registerOnConversationClick(onConversationClick());
-
-        mBinding.recyclerViewArchivedChats.setAdapter(mAdapter);
-        mBinding.recyclerViewArchivedChats.setHasFixedSize(true);
-        mBinding.recyclerViewArchivedChats.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    private BiConsumer<Integer, Boolean> onConversationClick() {
-        return (pos, isLongClick) -> {
-            Conversation conversation = mAdapter.getCurrentList().get(pos);
-
-            if (isLongClick) {
-                ConversationDialogFragment dialog = ConversationDialogFragment.newInstance(this, conversation);
-                dialog.show(getSupportFragmentManager(), null);
-            } else {
-                Intent conversationIntent = new Intent(this, ConversationActivity.class);
-                conversationIntent.putExtra("conversation", conversation);
-
-                activityResultLauncher.launch(conversationIntent, result -> {
-                    if (result != null && result.getResultCode() == Activity.RESULT_OK) {
-                        Intent intent = result.getData();
-
-                        if (intent != null) {
-                            // Change the conversation status to INBOX if updated
-                            Conversation updated = intent.getParcelableExtra("conversation");
-
-                            mViewModel.changeConversationStatusType(updated, ConversationStatusType.INBOX);
-                            removeConversationAdapterUI(updated);
-                        }
-                    }
-                });
-            }
-        };
-    }
-
-    @Override
-    public void onUnArchive(Conversation conversation) {
-        mViewModel.changeConversationStatusType(conversation, ConversationStatusType.INBOX);
-        removeConversationAdapterUI(conversation);
-    }
-
-    @Override
-    public void onDelete(Conversation conversation) {
-        mViewModel.delete(conversation);
-        removeConversationAdapterUI(conversation);
-    }
-
-    @Override
-    public void onMuteNotification(Conversation conversation) {
-
-    }
-
-    @Override
-    public void onCreateGroup(Conversation conversation) {
-
-    }
-
-    @Override
-    public void onLeaveGroup(Conversation conversation) {
-
-    }
-
-    @Override
-    public void onAddMember(Conversation conversation) {
-
-    }
-
-    @Override
-    public void onMarkUnread(Conversation conversation) {
-
-    }
-
-    @Override
-    public void onIgnore(Conversation conversation) {
-
-    }
-
-    private void removeConversationAdapterUI(Conversation conversation) {
-        mConversations.remove(conversation);
-        checkDataIsEmpty();
-        mAdapter.submitList(new ArrayList<>(mConversations));
-    }
-
-    private void checkDataIsEmpty() {
-        if (mConversations.isEmpty()) {
-            mBinding.recyclerViewArchivedChats.setVisibility(View.GONE);
-            mBinding.imageNoData.setVisibility(View.VISIBLE);
-            mBinding.textNoData.setVisibility(View.VISIBLE);
-        } else {
-            mBinding.recyclerViewArchivedChats.setVisibility(View.VISIBLE);
-            mBinding.imageNoData.setVisibility(View.GONE);
-            mBinding.textNoData.setVisibility(View.GONE);
-        }
     }
 }
